6384c998e9b541780b38dd5fe072738e2dbf26c5
karlinglee <459607025@qq.com>
2025-03-07 07:03:13 +0000

    Add date picker

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,17 +2,45 @@
 import Layout from "antd/lib/layout";
 
 import SideNav from "@/components/dashboard/side-nav";
+import { Select } from "antd";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { getDates } from "@/lib/utils";
 
 const { Sider, Header, Footer, Content } = Layout;
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const handleChange = (value: string) => {
+    const params = new URLSearchParams(searchParams);
+    const { start, end } = getDates(value);
+    params.set("start", start);
+    params.set("end", end);
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <Layout>
       <Sider>
         <SideNav />
       </Sider>
       <Layout>
-        <Header>Dashboard</Header>
+        <Header>
+          <Select
+            defaultValue="this_month"
+            style={{ width: 120 }}
+            onChange={handleChange}
+            options={[
+              { value: "this_month", label: "This month" },
+              { value: "last_30_days", label: "Last 30 days" },
+              { value: "this_year", label: "This year" },
+              { value: "last_12_months", label: "Last 12 months" },
+              { value: "from_start", label: "From start" },
+              { value: "custom", label: "Custom" },
+            ]}
+          />
+        </Header>
         <Content>{children}</Content>
         <Footer>Footer</Footer>
       </Layout>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -31,7 +31,7 @@ export const generateFullDates = (startDate: Date, endDate: Date) => {
   return dates;
 };
 
-export const formatDate = (date: Date) => {
+export const formatDate = (date: Date): string => {
   return date.toISOString().split("T")[0];
 };
 
@@ -41,6 +41,44 @@ export const getMonthFirstDate = (year: number, monthIndex: number) => {
 
 export const getMonthLastDate = (year: number, monthIndex: number) => {
   return new Date(year, monthIndex + 1, 0);
+};
+
+export const getDates = (type: string): { start: string; end: string } => {
+  const today = new Date();
+  const year = today.getFullYear();
+  const monthIndex = today.getMonth();
+  const day = today.getDate();
+
+  switch (type) {
+    case "last_30_days":
+      return {
+        start: formatDate(new Date(year, monthIndex - 1, day)),
+        end: formatDate(today),
+      };
+    case "this_year":
+      return {
+        start: formatDate(new Date(year, 0, 1)),
+        end: formatDate(new Date(year, 11, 31)),
+      };
+    case "last_12_months":
+      return {
+        start: formatDate(new Date(year - 1, monthIndex, day)),
+        end: formatDate(today),
+      };
+    case "from_start":
+      return {
+        start: formatDate(new Date("2000-01-01")),
+        end: formatDate(today),
+      };
+    // TODO: handle custom date
+    case "custom":
+    case "this_month":
+    default:
+      return {
+        start: formatDate(new Date(year, monthIndex, 1)),
+        end: formatDate(new Date(year, monthIndex + 1, 0)),
+      };
+  }
 };
 
 export const convertCurrency = (value: number, rate: number) => {
